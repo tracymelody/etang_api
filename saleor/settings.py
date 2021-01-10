cafdda011f410ea13b0424a2c6022cc898b8f163
JiaweiCui <jiawei.cui@smart-digital.de>
2021-01-10 02:17:07 +0100

    add paypal integration

--- saleor/settings.py
+++ saleor/settings.py
@@ -517,6 +517,7 @@ PLUGINS = [
     "saleor.payment.gateways.braintree.plugin.BraintreeGatewayPlugin",
     "saleor.payment.gateways.razorpay.plugin.RazorpayGatewayPlugin",
     "saleor.payment.gateways.adyen.plugin.AdyenGatewayPlugin",
+    "saleor.payment.gateways.paypal.plugin.PaypalGatewayPlugin",
     "saleor.plugins.invoicing.plugin.InvoicingPlugin",
 ]
 
